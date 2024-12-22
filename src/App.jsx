4b21006ffdb10fd25b019eaf9c6122138debcbce
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-22 16:59:47 -0300

    add form.jsx form.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './components/Banner/Banner.css'
 import {Banner} from "./components/Banner/index.jsx";
-import TextField from "./components/TextField/index.jsx";
+import Form from "./components/Form/index.jsx";
 
 function App() {
 
@@ -8,10 +8,7 @@ function App() {
         <>
             <div>
                 <Banner/>
-                <TextField label='Name' placeholder='Type your name'/>
-                <TextField label='Role' placeholder='Type your role'/>
-                <TextField label='Image' placeholder='Provide the image URL'/>
-                <TextField label='Team' placeholder='Type your team'/>
+                <Form/>
             </div>
         </>
     );
