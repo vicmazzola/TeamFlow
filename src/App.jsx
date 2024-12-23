c6d4624dc1705c02614b8cde2faf120bc334e7cd
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-23 17:01:33 -0300

    add Team.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './components/Banner/Banner.css'
 import {Banner} from "./components/Banner/index.jsx";
 import Form from "./components/Form/index.jsx";
+import Team from "./components/Team/index.jsx";
 import {useState} from "react";
 
 function App() {
@@ -17,7 +18,14 @@ function App() {
         <>
             <div>
                 <Banner/>
-                <Form collaboratorRegistered = {collaborator => newCollaborator(collaborator) } />
+                <Form collaboratorRegistered={collaborator => newCollaborator(collaborator)}/>
+                <Team name='Back-end'/>
+                <Team name='Front-end'/>
+                <Team name='Data Science'/>
+                <Team name='DevOps'/>
+                <Team name='UX and Design'/>
+                <Team name='Mobile'/>
+                <Team name='Innovation and Management'/>
             </div>
         </>
     );
